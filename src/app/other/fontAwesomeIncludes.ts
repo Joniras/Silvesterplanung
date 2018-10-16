781c63e7e776c44e64efc80250269256df86d9b9
Joniras <Josch73a>
2018-10-16 16:00:26 +0200

    adminSeite verbessert

--- src/app/other/fontAwesomeIncludes.ts
+++ src/app/other/fontAwesomeIncludes.ts
@@ -2,7 +2,7 @@ import {library} from '@fortawesome/fontawesome-svg-core';
 import {faSync} from '@fortawesome/free-solid-svg-icons/faSync';
 import {faFacebookF} from '@fortawesome/free-brands-svg-icons/faFacebookF';
 import {faGooglePlusG} from '@fortawesome/free-brands-svg-icons/faGooglePlusG';
-import {faBars, faHeart, faInfoCircle, faPlus, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
+import {faBars, faHeart, faInfoCircle, faPlus, faSignOutAlt, faTimes} from '@fortawesome/free-solid-svg-icons';
 import {faLock} from '@fortawesome/free-solid-svg-icons/faLock';
 
 library.add(
@@ -15,4 +15,5 @@ library.add(
   , faLock
   , faSignOutAlt
   , faInfoCircle
+  , faTimes
 );
